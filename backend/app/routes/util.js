94e765b3d520212e58487dae7af5f78d3c35b2eb
maxion <maxion03@gmail.com>
2020-11-17 00:59:40 +0300

    Fix(IP): fix getting IP

--- backend/app/routes/util.js
+++ backend/app/routes/util.js
@@ -1,9 +1,21 @@
 module.exports = function(app, db) {
   app.get('/ip', (req, res) => {
+    const LOCALHOST_IP = '127.0.0.1';
     const os = require('os');
     const networkInterfaces = os.networkInterfaces();
     const arr = Object.values(networkInterfaces);
-    const ip = arr[0][1].address;
+
+    let ipsArr = arr.reduce((result, arrItem) => {
+      result.push(...arrItem.map(arrItem => arrItem.address));
+      return result;
+    }, []);
+
+    ipsArr = ipsArr.filter(
+        ip => /^\d+\.\d+\.\d+\.\d+$/ig.test(ip) && ip !== LOCALHOST_IP);
+
+    const ip = ipsArr.length
+        ? ipsArr.join(' или ')
+        : null;
 
     res.send(ip);
   });
